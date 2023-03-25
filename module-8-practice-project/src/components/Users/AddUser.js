45faa023845d8918ff6aefcb7654f22e6546c442
Siddharth Bhat <dbgt4531@gmail.com>
2023-03-25 22:15:36 +0530

    created UI components
    1. Card
    2. Button

---
+++ module-8-practice-project/src/components/Users/AddUser.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from '../UI/Button';
+import Card from '../UI/Card';
+import classes from './AddUser.module.css'
+
+const AddUser = (props) => {
+    const addUserHandler = (event) => {
+        event.preventDefault();
+    };
+    return (
+        <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" />
+                <label htmlFor="age">Age (Years)</label>
+                <input type="number" id="age" />
+                <Button type="submit">Add User</Button>
+            </form>
+        </Card>
+    )
+};
+
+export default AddUser;
