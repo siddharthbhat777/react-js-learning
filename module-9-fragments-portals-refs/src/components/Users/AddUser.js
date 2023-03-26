89154f09eba4a77fe89fb686ad7ff352c20224c7
Siddharth Bhat <dbgt4531@gmail.com>
2023-03-26 13:18:53 +0530

    used wrapper component to replace div to avoid unnecessary rendering of dom elements

--- module-9-fragments-portals-refs/src/components/Users/AddUser.js
+++ module-9-fragments-portals-refs/src/components/Users/AddUser.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Wrapper from '../Helpers/Wrapper';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 import ErrorModal from '../UI/ErrorModal';
@@ -42,7 +43,7 @@ const AddUser = (props) => {
     };
 
     return (
-        <div>
+        <Wrapper>
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
@@ -53,7 +54,7 @@ const AddUser = (props) => {
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
+        </Wrapper>
     )
 };
 
