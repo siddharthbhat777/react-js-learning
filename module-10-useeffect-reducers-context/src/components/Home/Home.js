43697867e688e3f22368cfda4131ba5648dfca67
Siddharth Bhat <dbgt4531@gmail.com>
2023-03-27 10:43:53 +0530

    created one central context
    Meaning: the provider will be defined in the context file itself instead of normal app files

--- module-10-useeffect-reducers-context/src/components/Home/Home.js
+++ module-10-useeffect-reducers-context/src/components/Home/Home.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AuthContext from '../../store/auth-context';
 import Button from '../UI/Button/Button';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
 
 const Home = (props) => {
+  const ctx = useContext(AuthContext);
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
-      <Button onClick={props.onLogout}>Logout</Button> {/* can use context api here but not need to because this is the child component of the component which defines logout functionality i.e. App.js */}
+      <Button onClick={ctx.onLogout}>Logout</Button> {/* can use context api here but not need to because this is the child component of the component which defines logout functionality i.e. App.js */}
     </Card>
   );
 };
--- module-10-useeffect-reducers-context/src/components/Login/Login.js
+++ module-10-useeffect-reducers-context/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useReducer, useState } from 'react';
+import React, { useContext, useEffect, useReducer, useState } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -33,6 +34,8 @@ const Login = (props) => {
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, { value: '', isValid: null });
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: '', isValid: null });
+
+  const ctx = useContext(AuthContext);
 
   useEffect(() => {
     console.log('EFFECT RUNNING');
@@ -81,7 +84,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    ctx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
