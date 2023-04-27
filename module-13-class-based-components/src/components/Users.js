246376d72b843189519f92d54d9f147c04a53409
Siddharth Bhat <dbgt4531@gmail.com>
2023-04-27 17:14:51 +0530

    used state in class based component

--- module-13-class-based-components/src/components/Users.js
+++ module-13-class-based-components/src/components/Users.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Component, useState } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
@@ -9,7 +9,49 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Aniruddha' },
 ];
 
-const Users = () => {
+class Users extends Component {
+  constructor() {
+    super(); // when you use a constructor for your class you use keyword 'super'
+    // your state in class based component will always be an OBJECT and also that property has to be named as 'state'
+    this.state = {
+      showUsers: true,
+      moreState: 'Test'
+      // nested: {},
+      // data: []
+    };
+  }
+
+  toggleUsersHandler() {
+    // 'setState' is a special method provided by react and it also always takes in OBJECT
+    // this.setState({showUsers: false}); // if you only change 1 state then your second state will be kept as it is
+    // OR
+    this.setState((curState) => {
+      return { showUsers: !curState.showUsers }
+    });
+  }
+
+  render() {
+    // can add helper constants in render method
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}> {/* bind: sometimes we this keyword dosen't work while handling events i.e. dosen't refer to the class context, so the reason to explicitly reference the context we use bind keyword */}
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+/* const Users = () => {
   const [showUsers, setShowUsers] = useState(true);
 
   const toggleUsersHandler = () => {
@@ -32,6 +74,6 @@ const Users = () => {
       {showUsers && usersList}
     </div>
   );
-};
+}; */
 
 export default Users;
