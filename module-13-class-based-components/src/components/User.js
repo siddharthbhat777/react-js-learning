55f4a5e71b037b45aca247deae7d110f5107c818
Siddharth Bhat <dbgt4531@gmail.com>
2023-04-27 15:57:33 +0530

    created first class based component

--- module-13-class-based-components/src/components/User.js
+++ module-13-class-based-components/src/components/User.js
@@ -1,7 +1,15 @@
+import { Component } from 'react';
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+class User extends Component {
+  // constructor() {} -- if you want to initialize anything do here
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
